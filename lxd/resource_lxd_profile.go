89cead93f33c9418103f107ecf84a6621575aee4
Robbert Müller <spam.me@grols.ch>
2017-09-20 17:06:48 +0200

    switch id syntax and parsing
    use [remote:]name syntax instead of name[:remote] to better match with
    the lxd native syntax for specifying a remote name in the identifier.
    
    Switched to ParseRemote from github.com/lxd/lxc/config/remote

--- lxd/resource_lxd_profile.go
+++ lxd/resource_lxd_profile.go
@@ -2,7 +2,6 @@ package lxd
 
 import (
 	"log"
-	"strings"
 
 	"github.com/hashicorp/terraform/helper/schema"
 
@@ -230,13 +229,15 @@ func resourceLxdProfileExists(d *schema.ResourceData, meta interface{}) (exists 
 
 func resourceLxdProfileImport(d *schema.ResourceData, meta interface{}) ([]*schema.ResourceData, error) {
 	p := meta.(*LxdProvider)
+	remote, name, err := p.Config.ParseRemote(d.Id())
 
-	ids := strings.SplitN(d.Id(), ":", 2)
-	name := ids[0]
+	if err != nil {
+		return nil, err
+	}
+
 	d.SetId(name)
-
-	if len(ids) == 2 {
-		d.Set("remote", ids[1])
+	if p.Config.DefaultRemote != remote {
+		d.Set("remote", remote)
 	}
 
 	server, err := p.GetContainerServer(p.selectRemote(d))
