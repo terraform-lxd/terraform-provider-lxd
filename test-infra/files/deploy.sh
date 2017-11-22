cb7c13bff5ae4478e4cdb92a3f3e2c4ad69f06ef
Joe Topjian <joe@topjian.net>
2017-11-22 06:54:07 +0000

    Updating test-infra files

--- test-infra/files/deploy.sh
+++ test-infra/files/deploy.sh
@@ -5,34 +5,39 @@ vol=$(curl http://169.254.169.254/latest/meta-data/block-device-mapping/ebs0)
 if [[ $vol == *404* ]]; then
   # This is AWS. Re-use the already attached ephemeral.
   sudo umount /mnt
-  sudo mkfs.btrfs -f /dev/xvdb
-  sudo mount -a
-  vol=/mnt/lxd
+  vol=/dev/xvdb
+  sudo wipefs -a $vol
+  sudo sed -i -e '/xvdb/d' /etc/fstab
 fi
 
-sudo add-apt-repository -y ppa:ubuntu-lxc/lxd-git-master
 sudo apt-get update -qq
-sudo apt-get install -y lxd
+sudo apt-get install -y snapd
 sudo apt-get install -y build-essential
-sudo lxc config set core.https_address [::]
-sudo lxc config set core.trust_password the-password
-#sudo lxc storage create default dir source=/mnt
-sudo lxc storage create default btrfs source=$vol
-sudo lxc profile device add default root disk path=/ pool=default
-sudo lxc network create lxdbr0 ipv6.address=none ipv4.address=192.168.244.1/24 ipv4.nat=true
-sudo lxc network attach-profile lxdbr0 default eth0
-sudo lxc image copy images:ubuntu/xenial/amd64 local: --alias ubuntu
+sudo snap install lxd
+
+_lxc="/snap/bin/lxc"
+_lxd="/snap/bin/lxd"
+sudo $_lxd waitready --timeout 60
+sudo $_lxc config set core.https_address [::]
+sudo $_lxc config set core.trust_password the-password
+#sudo $_lxc storage create default dir source=/mnt
+sudo $_lxc storage create default btrfs source=$vol
+sudo $_lxc profile device add default root disk path=/ pool=default
+sudo $_lxc network create lxdbr0 ipv6.address=none ipv4.address=192.168.244.1/24 ipv4.nat=true
+sudo $_lxc network attach-profile lxdbr0 default eth0
+sudo $_lxc image copy images:ubuntu/xenial/amd64 local: --alias ubuntu
+sudo $_lxc image copy images:alpine/3.5 local: --alias alpine
 sudo usermod -a -G lxd ubuntu
-sudo chown -R ubuntu: /home/ubuntu/.config
 
 sudo wget -O /usr/local/bin/gimme https://raw.githubusercontent.com/travis-ci/gimme/master/gimme
 sudo chmod +x /usr/local/bin/gimme
 
 cat >> ~/.bashrc <<EOF
-eval "\$(/usr/local/bin/gimme 1.8)"
+eval "\$(/usr/local/bin/gimme 1.9)"
 export GOPATH=\$HOME/go
-export PATH=\$PATH:\$GOROOT/bin:\$GOPATH/bin
+export PATH=/snap/bin:\$PATH:\$GOROOT/bin:\$GOPATH/bin
 
+export LXD_REMOTE=travis
 export LXD_ADDR=localhost
 export LXD_PORT=8443
 export LXD_GENERATE_CLIENT_CERTS=true
@@ -41,12 +46,11 @@ export LXD_SCHEME=https
 export LXD_PASSWORD=the-password
 EOF
 
-eval "$(/usr/local/bin/gimme 1.8)"
+eval "$(/usr/local/bin/gimme 1.9)"
 export GOPATH=$HOME/go
 export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
 
 go get github.com/sl1pm4t/terraform-provider-lxd
-go get github.com/sl1pm4t/lxdhelpers
 go get github.com/gosexy/gettext
 go get github.com/dustinkirkland/golang-petname
 
--- test-infra/openstack/main.tf
+++ test-infra/openstack/main.tf
@@ -1,6 +1,9 @@
 variable "key_name" {}
+
 variable "private_key" {}
+
 variable "image_id" {}
+
 variable "network_id" {}
 
 variable "pool" {
@@ -12,7 +15,7 @@ variable "flavor" {
 }
 
 resource "openstack_networking_secgroup_v2" "lxd_acc_tests" {
-  name = "lxd_acc_tests"
+  name        = "lxd_acc_tests"
   description = "Rules for lxd acceptance tests"
 }
 
@@ -22,50 +25,50 @@ resource "openstack_networking_floatingip_v2" "lxd_acc_tests" {
 
 resource "openstack_networking_secgroup_rule_v2" "lxd_acc_tests_rule_1" {
   security_group_id = "${openstack_networking_secgroup_v2.lxd_acc_tests.id}"
-  direction = "ingress"
-  ethertype = "IPv4"
-  protocol = "tcp"
-  port_range_min = 1
-  port_range_max = 65535
-  remote_ip_prefix = "0.0.0.0/0"
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 1
+  port_range_max    = 65535
+  remote_ip_prefix  = "0.0.0.0/0"
 }
 
 resource "openstack_networking_secgroup_rule_v2" "lxd_acc_tests_rule_2" {
   security_group_id = "${openstack_networking_secgroup_v2.lxd_acc_tests.id}"
-  direction = "ingress"
-  ethertype = "IPv6"
-  protocol = "tcp"
-  port_range_min = 1
-  port_range_max = 65535
-  remote_ip_prefix = "::/0"
+  direction         = "ingress"
+  ethertype         = "IPv6"
+  protocol          = "tcp"
+  port_range_min    = 1
+  port_range_max    = 65535
+  remote_ip_prefix  = "::/0"
 }
 
 resource "openstack_networking_secgroup_rule_v2" "lxd_acc_tests_rule_3" {
   security_group_id = "${openstack_networking_secgroup_v2.lxd_acc_tests.id}"
-  direction = "ingress"
-  ethertype = "IPv4"
-  protocol = "udp"
-  port_range_min = 1
-  port_range_max = 65535
-  remote_ip_prefix = "0.0.0.0/0"
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "udp"
+  port_range_min    = 1
+  port_range_max    = 65535
+  remote_ip_prefix  = "0.0.0.0/0"
 }
 
 resource "openstack_networking_secgroup_rule_v2" "lxd_acc_tests_rule_4" {
   security_group_id = "${openstack_networking_secgroup_v2.lxd_acc_tests.id}"
-  direction = "ingress"
-  ethertype = "IPv6"
-  protocol = "udp"
-  port_range_min = 1
-  port_range_max = 65535
-  remote_ip_prefix = "::/0"
+  direction         = "ingress"
+  ethertype         = "IPv6"
+  protocol          = "udp"
+  port_range_min    = 1
+  port_range_max    = 65535
+  remote_ip_prefix  = "::/0"
 }
 
 resource "openstack_networking_secgroup_rule_v2" "lxd_acc_tests_rule_5" {
   security_group_id = "${openstack_networking_secgroup_v2.lxd_acc_tests.id}"
-  direction = "ingress"
-  ethertype = "IPv4"
-  protocol = "icmp"
-  remote_ip_prefix = "0.0.0.0/0"
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "icmp"
+  remote_ip_prefix  = "0.0.0.0/0"
 }
 
 resource "openstack_blockstorage_volume_v2" "lxd_acc_tests" {
@@ -75,17 +78,17 @@ resource "openstack_blockstorage_volume_v2" "lxd_acc_tests" {
 
 resource "openstack_networking_secgroup_rule_v2" "lxd_acc_tests_rule_6" {
   security_group_id = "${openstack_networking_secgroup_v2.lxd_acc_tests.id}"
-  direction = "ingress"
-  ethertype = "IPv6"
-  protocol = "icmp"
-  remote_ip_prefix = "::/0"
+  direction         = "ingress"
+  ethertype         = "IPv6"
+  protocol          = "icmp"
+  remote_ip_prefix  = "::/0"
 }
 
 resource "openstack_compute_instance_v2" "lxd_acc_tests" {
-  name = "lxd_acc_tests"
-  image_id = "${var.image_id}"
+  name        = "lxd_acc_tests"
+  image_id    = "${var.image_id}"
   flavor_name = "${var.flavor}"
-  key_pair = "${var.key_name}"
+  key_pair    = "${var.key_name}"
 
   security_groups = ["${openstack_networking_secgroup_v2.lxd_acc_tests.name}"]
 
@@ -101,15 +104,15 @@ resource "openstack_compute_floatingip_associate_v2" "lxd_acc_tests" {
 
 resource "openstack_compute_volume_attach_v2" "lxd_acc_tests" {
   instance_id = "${openstack_compute_instance_v2.lxd_acc_tests.id}"
-  volume_id = "${openstack_blockstorage_volume_v2.lxd_acc_tests.id}"
+  volume_id   = "${openstack_blockstorage_volume_v2.lxd_acc_tests.id}"
 }
 
 resource "null_resource" "lxd_acc_tests" {
   connection {
-    user = "ubuntu"
-    type = "ssh"
+    user        = "ubuntu"
+    type        = "ssh"
     private_key = "${file(var.private_key)}"
-    host = "${openstack_compute_floatingip_associate_v2.lxd_acc_tests.floating_ip}"
+    host        = "${openstack_compute_floatingip_associate_v2.lxd_acc_tests.floating_ip}"
   }
 
   provisioner "remote-exec" {
