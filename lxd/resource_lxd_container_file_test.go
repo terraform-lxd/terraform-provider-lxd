b1018c95fa1099bc5d748a78ddb8f9e73f81beb9
Joe Topjian <joe@topjian.net>
2017-11-26 06:23:01 +0000

    Container File Resource
    This commit adds the lxd_container_file resource which can be used
    to manage individual files in a container.

---
+++ lxd/resource_lxd_container_file_test.go
@@ -0,0 +1,122 @@
+package lxd
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/dustinkirkland/golang-petname"
+
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+
+	"github.com/lxc/lxd/client"
+)
+
+func TestAccContainerFile_content(t *testing.T) {
+	var file lxd.ContainerFileResponse
+
+	containerName := strings.ToLower(petname.Generate(2, "-"))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccContainerFile_content(containerName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccContainerFileExists(t, "lxd_container_file.file1", &file),
+					resource.TestCheckResourceAttr("lxd_container_file.file1", "create_directories", "true"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccContainerFile_source(t *testing.T) {
+	var file lxd.ContainerFileResponse
+
+	containerName := strings.ToLower(petname.Generate(2, "-"))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccContainerFile_source(containerName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccContainerFileExists(t, "lxd_container_file.file1", &file),
+					resource.TestCheckResourceAttr("lxd_container_file.file1", "create_directories", "true"),
+				),
+			},
+		},
+	})
+}
+
+func testAccContainerFileExists(t *testing.T, n string, file *lxd.ContainerFileResponse) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No ID is set")
+		}
+
+		p := testAccProvider.Meta().(*lxdProvider)
+		v, targetFile := newFileIDFromResourceID(rs.Primary.ID)
+		remote, containerName, err := p.Config.ParseRemote(v)
+
+		client, err := p.GetContainerServer(remote)
+		if err != nil {
+			return err
+		}
+
+		_, f, err := client.GetContainerFile(containerName, targetFile)
+		if err != nil {
+			return err
+		}
+
+		if f != nil {
+			*file = *f
+			return nil
+		}
+
+		return fmt.Errorf("Container file not found: %s", rs.Primary.ID)
+	}
+}
+
+func testAccContainerFile_content(name string) string {
+	return fmt.Sprintf(`
+resource "lxd_container" "container1" {
+  name = "%s"
+  image = "images:alpine/3.5/amd64"
+  profiles = ["default"]
+}
+
+resource "lxd_container_file" "file1" {
+  container_name = "${lxd_container.container1.name}"
+  target_file = "/foo/bar.txt"
+  content = "Hello, World!\n"
+  create_directories = true
+}
+	`, name)
+}
+
+func testAccContainerFile_source(name string) string {
+	return fmt.Sprintf(`
+resource "lxd_container" "container1" {
+  name = "%s"
+  image = "images:alpine/3.5/amd64"
+  profiles = ["default"]
+}
+
+resource "lxd_container_file" "file1" {
+  container_name = "${lxd_container.container1.name}"
+  target_file = "/foo/bar.txt"
+	source = "test-fixtures/test-file.txt"
+  create_directories = true
+}
+	`, name)
+}
