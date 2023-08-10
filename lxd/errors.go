3d3c80977317354e843f37a2508972fa1903488c
Din Music <din.music@canonical.com>
2023-08-10 16:17:55 +0200

    lxd/errors: Add function that checks if error status is not found

--- lxd/errors.go
+++ lxd/errors.go
@@ -2,8 +2,20 @@ package lxd
 
 import (
 	"errors"
+	"net/http"
+
+	"github.com/canonical/lxd/shared/api"
 )
 
 var errNetworksNotImplemented = errors.New("This LXD server does not support " +
 	"the creation of networks. You must be running LXD 2.3 or later for this " +
 	"feature.")
+
+func isNotFoundError(err error) bool {
+	// For LXD versions below "4.0".
+	if err.Error() == "No such object" || err.Error() == "not found" {
+		return true
+	}
+
+	return api.StatusErrorCheck(err, http.StatusNotFound)
+}
