408226967a05d7c48cd3f8dc2edc54817cd0afd7
Din Music <din.music@canonical.com>
2023-10-27 13:09:43 +0200

    lxd/container: Fix ignored error in instance exists check
    Signed-off-by: Din Music <din.music@canonical.com>

--- lxd/resource_lxd_container.go
+++ lxd/resource_lxd_container.go
@@ -781,7 +781,7 @@ func resourceLxdContainerExists(d *schema.ResourceData, meta interface{}) (exist
 		exists = true
 	}
 
-	return exists, nil
+	return
 }
 
 func resourceLxdContainerImport(d *schema.ResourceData, meta interface{}) ([]*schema.ResourceData, error) {
