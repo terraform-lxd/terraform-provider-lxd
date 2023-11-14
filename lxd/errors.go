7075e4093fa9a0ccf4073cd278052d3e3fd5baf2
Simon Deziel <simon.deziel@canonical.com>
2023-11-14 16:10:47 -0500

    lxd/error: drop compat code for unsupported LXD versions
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- lxd/errors.go
+++ lxd/errors.go
@@ -1,17 +1,11 @@
 package lxd
 
 import (
-	"errors"
 	"net/http"
 
 	"github.com/canonical/lxd/shared/api"
 )
 
 func isNotFoundError(err error) bool {
-	// For LXD versions below "4.0".
-	if err.Error() == "No such object" || err.Error() == "not found" {
-		return true
-	}
-
 	return api.StatusErrorCheck(err, http.StatusNotFound)
 }
