d919b00bab273f3313526312bab53b8704fe10ef
Din Music <din.music@canonical.com>
2023-11-16 13:55:24 +0100

    internal/project/test: Add tests for project resource
    Signed-off-by: Din Music <din.music@canonical.com>

---
+++ internal/project/resource_project_test.go
@@ -0,0 +1,155 @@
+package project_test
+
+import (
+	"fmt"
+	"testing"
+
+	petname "github.com/dustinkirkland/golang-petname"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/terraform-lxd/terraform-provider-lxd/internal/provider/acctest"
+)
+
+// //At a high level, the first basic test for a resource should establish the following:
+// //- Terraform can plan and apply a common resource configuration without error.
+// //- Verify the expected attributes are saved to state, and contain the values expected.
+// //- Verify the values in the remote API/Service for the resource match what is stored in state.
+// //- Verify that a subsequent terraform plan does not produce a diff/change.
+
+func TestAccProject_basic(t *testing.T) {
+	// var project api.Project
+	projectName := petname.Generate(2, "-")
+
+	// https://github.com/hashicorp/terraform-plugin-sdk/blob/main/helper/resource/testing.go
+	// https://developer.hashicorp.com/terraform/plugin/sdkv2/testing/acceptance-tests/testcase
+	resource.Test(t, resource.TestCase{
+		// PreCheck, if non-nil, will be called before any test steps are
+		// executed. It will only be executed in the case that the steps
+		// would run, so it can be used for some validation before running
+		// acceptance tests, such as verifying that keys are setup.
+		PreCheck:                 func() { acctest.PreCheck(t) },
+		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories,
+		// Terraform runs plan, apply, refresh, and then final plan for each TestStep in the TestCase
+		// If the last plan results in a non-empty plan, Terraform will exit with an error.
+		Steps: []resource.TestStep{
+			{
+				Config: testAccProject_basic(projectName),
+				Check: resource.ComposeTestCheckFunc(
+					// testAccProjectRunning(t, "lxd_project.project0", &project),
+					// resource.TestCheckResourceAttr("lxd_project.project0", "id", projectName),
+
+					// Check user-defined attributes.
+					resource.TestCheckResourceAttr("lxd_project.project0", "name", projectName),
+					resource.TestCheckResourceAttr("lxd_project.project0", "description", "Terraform provider test project"),
+					// Check config_state.
+					resource.TestCheckResourceAttr("lxd_project.project0", "config_state.features.images", "true"),
+					resource.TestCheckResourceAttr("lxd_project.project0", "config_state.features.profiles", "true"),
+					resource.TestCheckResourceAttr("lxd_project.project0", "config_state.features.storage.volumes", "true"),
+					resource.TestCheckResourceAttr("lxd_project.project0", "config_state.features.storage.buckets", "true"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccProject_config(t *testing.T) {
+	// var project api.Project
+	projectName := petname.Generate(2, "-")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(t) },
+		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccProject_config(projectName),
+				Check: resource.ComposeTestCheckFunc(
+					// testAccProjectRunning(t, "lxd_project.project1", &project),
+
+					// Check user-defined attributes.
+					resource.TestCheckResourceAttr("lxd_project.project1", "name", projectName),
+					resource.TestCheckResourceAttr("lxd_project.project1", "config.features.images", "false"),
+					resource.TestCheckResourceAttr("lxd_project.project1", "config.features.profiles", "false"),
+					// Check config_state.
+					resource.TestCheckResourceAttr("lxd_project.project1", "config_state.features.images", "false"),
+					resource.TestCheckResourceAttr("lxd_project.project1", "config_state.features.profiles", "false"),
+					resource.TestCheckResourceAttr("lxd_project.project1", "config_state.features.storage.volumes", "true"),
+					resource.TestCheckResourceAttr("lxd_project.project1", "config_state.features.storage.buckets", "true"),
+
+					// testAccProjectConfig(&project, "features.images", "false"),
+					// testAccProjectConfig(&project, "features.profiles", "false"),
+					// testAccProjectConfig(&project, "features.storage.volumes", "false"),
+				),
+			},
+		},
+	})
+}
+
+// func testAccProjectRunning(t *testing.T, n string, project *api.Project) resource.TestCheckFunc {
+// 	return func(s *terraform.State) error {
+// 		rs, ok := s.RootModule().Resources[n]
+// 		if !ok {
+// 			return fmt.Errorf("Not found: %s", n)
+// 		}
+
+// 		if rs.Primary.ID == "" {
+// 			return fmt.Errorf("No ID is set")
+// 		}
+
+// 		client, err := testAccProvider.Meta().(*lxdProvider).GetInstanceServer("")
+// 		if err != nil {
+// 			return err
+// 		}
+// 		p, _, err := client.GetProject(rs.Primary.ID)
+// 		if err != nil {
+// 			return err
+// 		}
+
+// 		if p != nil {
+// 			*project = *p
+// 			return nil
+// 		}
+
+// 		return fmt.Errorf("Project not found: %s", rs.Primary.ID)
+// 	}
+// }
+
+// func testAccProjectConfig(project *api.Project, k, v string) resource.TestCheckFunc {
+// 	return func(s *terraform.State) error {
+// 		if project.Config == nil {
+// 			return fmt.Errorf("No config")
+// 		}
+
+// 		for key, value := range project.Config {
+// 			if k != key {
+// 				continue
+// 			}
+
+// 			if v == value {
+// 				return nil
+// 			}
+
+// 			return fmt.Errorf("Bad value for %s: %s", k, value)
+// 		}
+
+// 		return fmt.Errorf("Config not found: %s", k)
+// 	}
+// }
+
+func testAccProject_basic(name string) string {
+	return fmt.Sprintf(`
+resource "lxd_project" "project0" {
+  name = "%s"
+  description = "Terraform provider test project"
+}`, name)
+}
+
+func testAccProject_config(name string) string {
+	return fmt.Sprintf(`
+resource "lxd_project" "project1" {
+  name = "%s"
+  description = "Terraform provider test project"
+  config = {
+	"features.images" = false
+	"features.profiles" = false
+  }
+}`, name)
+}
