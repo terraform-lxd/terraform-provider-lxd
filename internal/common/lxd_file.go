0401b090a7e925933c634cb03b398bd3ff448f8f
Din Music <din.music@canonical.com>
2023-11-19 00:20:38 +0100

    internal/common/file: Add instance file upload and delete
    Signed-off-by: Din Music <din.music@canonical.com>

---
+++ internal/common/lxd_file.go
@@ -0,0 +1,224 @@
+package common
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path"
+	"path/filepath"
+	"strconv"
+	"strings"
+
+	lxd "github.com/canonical/lxd/client"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/mitchellh/go-homedir"
+	"github.com/terraform-lxd/terraform-provider-lxd/internal/errors"
+)
+
+type LxdFileModel struct {
+	Content    types.String `tfsdk:"content"`
+	Source     types.String `tfsdk:"source"`
+	TargetFile types.String `tfsdk:"target_file"`
+	UserID     types.Int64  `tfsdk:"uid"`
+	GroupID    types.Int64  `tfsdk:"gid"`
+	Mode       types.String `tfsdk:"mode"`
+	CreateDirs types.Bool   `tfsdk:"create_directories"`
+	Append     types.Bool   `tfsdk:"append"`
+}
+
+func (f *LxdFileModel) String() string {
+	return fmt.Sprintf(
+		`
+  Content: %q
+  Source:  %q
+  Target:  %q
+`,
+		f.Content.String(),
+		f.Source.String(),
+		f.TargetFile.String(),
+	)
+}
+
+// ToFileMap converts files from types.Set into map[string]LxdFileModel.
+func ToFileMap(ctx context.Context, fileSet types.Set) (map[string]LxdFileModel, diag.Diagnostics) {
+	if fileSet.IsNull() || fileSet.IsUnknown() {
+		return make(map[string]LxdFileModel), nil
+	}
+
+	files := make([]LxdFileModel, 0, len(fileSet.Elements()))
+	diags := fileSet.ElementsAs(ctx, &files, false)
+	if diags.HasError() {
+		return nil, diags
+	}
+
+	// Convert list into map.
+	fileMap := make(map[string]LxdFileModel, len(files))
+	for _, f := range files {
+		fileMap[f.TargetFile.ValueString()] = f
+	}
+
+	return fileMap, diags
+}
+
+// ToFileSetType converts files from a map[string]LxdFileModel into types.Set.
+func ToFileSetType(ctx context.Context, fileMap map[string]LxdFileModel) (types.Set, diag.Diagnostics) {
+	files := make([]LxdFileModel, 0, len(fileMap))
+	for _, v := range fileMap {
+		files = append(files, v)
+	}
+
+	return types.SetValueFrom(ctx, types.ObjectType{}, files)
+}
+
+// InstanceFileDelete deletes a file from an instance.
+func InstanceFileDelete(server lxd.InstanceServer, instanceName string, targetFile string) error {
+	targetFile, err := toAbsFilePath(targetFile)
+	if err != nil {
+		return err
+	}
+
+	err = server.DeleteInstanceFile(instanceName, targetFile)
+	if err != nil && !errors.IsNotFoundError(err) {
+		return err
+	}
+
+	return nil
+}
+
+// InstanceFileUpload uploads a file to an instance.
+func InstanceFileUpload(server lxd.InstanceServer, instanceName string, file LxdFileModel) error {
+	content := file.Content.ValueString()
+	source := file.Source.ValueString()
+
+	if content != "" && source != "" {
+		return fmt.Errorf("File %q and %q are mutually exclusive.", "content", "source")
+	}
+
+	targetFile, err := toAbsFilePath(file.TargetFile.ValueString())
+	if err != nil {
+		return err
+	}
+
+	mode, err := strconv.ParseUint(file.Mode.ValueString(), 8, 32)
+	if err != nil {
+		return fmt.Errorf("Failed to parse file mode: %v", err)
+	}
+
+	// Build the file creation request, without the content.
+	args := &lxd.InstanceFileArgs{
+		Type: "file",
+		Mode: int(mode),
+		UID:  file.UserID.ValueInt64(),
+		GID:  file.GroupID.ValueInt64(),
+	}
+
+	if file.Append.ValueBool() {
+		args.WriteMode = "append"
+	} else {
+		args.WriteMode = "overwrite"
+	}
+
+	// If content was specified, read the string.
+	if content != "" {
+		args.Content = strings.NewReader(content)
+	}
+
+	// If a source was specified, read the contents of the source file.
+	if source != "" {
+		path, err := homedir.Expand(source)
+		if err != nil {
+			return fmt.Errorf("Unable to determine source file path: %v", err)
+		}
+
+		f, err := os.Open(path)
+		if err != nil {
+			return fmt.Errorf("Unable to read source file: %v", err)
+		}
+		defer f.Close()
+
+		args.Content = f
+	}
+
+	if file.CreateDirs.ValueBool() {
+		err := recursiveMkdir(server, instanceName, path.Dir(targetFile), *args)
+		if err != nil {
+			return fmt.Errorf("Could not create directories for file %q: %v", targetFile, err)
+		}
+	}
+
+	err = server.CreateInstanceFile(instanceName, targetFile, *args)
+	if err != nil {
+		return fmt.Errorf("Could not upload file %q: %v", targetFile, err)
+	}
+
+	return nil
+}
+
+// recursiveMkdir recursively creates directories on target instance.
+// This was copied almost as-is from github.com/canonical/lxd/blob/main/lxc/file.go.
+func recursiveMkdir(server lxd.InstanceServer, instanceName string, p string, args lxd.InstanceFileArgs) error {
+	// Special case, every instance has a /, so there is nothing to do.
+	if p == "/" {
+		return nil
+	}
+
+	// Remove trailing "/" e.g. /A/B/C/. Otherwise we will end up with an
+	// empty array entry "" which will confuse the Mkdir() loop below.
+	pclean := filepath.Clean(p)
+	parts := strings.Split(pclean, "/")
+	i := len(parts)
+
+	for ; i >= 1; i-- {
+		cur := filepath.Join(parts[:i]...)
+		_, resp, err := server.GetInstanceFile(instanceName, cur)
+		if err != nil {
+			continue
+		}
+
+		if resp.Type != "directory" {
+			return fmt.Errorf("%s is not a directory", cur)
+		}
+
+		i++
+		break
+	}
+
+	// Use same arguments as for file upload, only change file type.
+	dirArgs := lxd.InstanceFileArgs{
+		Type: "directory",
+		Mode: args.Mode,
+		UID:  args.UID,
+		GID:  args.GID,
+	}
+
+	for ; i <= len(parts); i++ {
+		cur := filepath.Join(parts[:i]...)
+		if cur == "" {
+			continue
+		}
+
+		err := server.CreateInstanceFile(instanceName, cur, dirArgs)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// toAbsFilePath returns absolute path of the given path and ensures that
+// the path is not a directory.
+func toAbsFilePath(filePath string) (string, error) {
+	targetFile, err := filepath.Abs(filePath)
+	if err != nil {
+		return "", fmt.Errorf("Failed to determine absoulute target file path: %v", err)
+	}
+
+	isDir := strings.HasSuffix(targetFile, "/")
+	if isDir {
+		return "", fmt.Errorf("Target file %q cannot be a directory: %v", targetFile, err)
+	}
+
+	return targetFile, nil
+}
