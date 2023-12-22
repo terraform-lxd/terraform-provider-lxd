64908408569e9f93c908e9a862a94884db80e03f
Simon Deziel <simon.deziel@canonical.com>
2023-12-22 11:03:07 -0500

    internal/image/resource_cached_image_test: use TestCachedImageSourceRemote and TestCachedImageSourceImage variables
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- internal/image/resource_cached_image_test.go
+++ internal/image/resource_cached_image_test.go
@@ -19,8 +19,8 @@ func TestAccCachedImage_basic(t *testing.T) {
 			{
 				Config: testAccCachedImage_basic(),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("lxd_cached_image.img1", "source_remote", "ubuntu-minimal-daily"),
-					resource.TestCheckResourceAttr("lxd_cached_image.img1", "source_image", "22.04"),
+					resource.TestCheckResourceAttr("lxd_cached_image.img1", "source_remote", acctest.TestCachedImageSourceRemote),
+					resource.TestCheckResourceAttr("lxd_cached_image.img1", "source_image", acctest.TestCachedImageSourceImage),
 					resource.TestCheckResourceAttr("lxd_cached_image.img1", "copy_aliases", "true"),
 					resource.TestCheckResourceAttr("lxd_cached_image.img1", "copied_aliases.#", "10"),
 				),
@@ -37,8 +37,8 @@ func TestAccCachedImage_basicVM(t *testing.T) {
 			{
 				Config: testAccCachedImage_basicVM(),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("lxd_cached_image.img1vm", "source_remote", "ubuntu-minimal-daily"),
-					resource.TestCheckResourceAttr("lxd_cached_image.img1vm", "source_image", "22.04"),
+					resource.TestCheckResourceAttr("lxd_cached_image.img1vm", "source_remote", acctest.TestCachedImageSourceRemote),
+					resource.TestCheckResourceAttr("lxd_cached_image.img1vm", "source_image", acctest.TestCachedImageSourceImage),
 					resource.TestCheckResourceAttr("lxd_cached_image.img1vm", "copy_aliases", "true"),
 					resource.TestCheckResourceAttr("lxd_cached_image.img1vm", "copied_aliases.#", "10"),
 					resource.TestCheckResourceAttr("lxd_cached_image.img1vm", "type", "virtual-machine"),
@@ -59,8 +59,8 @@ func TestAccCachedImage_alias(t *testing.T) {
 			{
 				Config: testAccCachedImage_aliases(alias1, alias2),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("lxd_cached_image.img2", "source_remote", "ubuntu-minimal-daily"),
-					resource.TestCheckResourceAttr("lxd_cached_image.img2", "source_image", "22.04"),
+					resource.TestCheckResourceAttr("lxd_cached_image.img2", "source_remote", acctest.TestCachedImageSourceRemote),
+					resource.TestCheckResourceAttr("lxd_cached_image.img2", "source_image", acctest.TestCachedImageSourceImage),
 					resource.TestCheckResourceAttr("lxd_cached_image.img2", "copy_aliases", "false"),
 					resource.TestCheckResourceAttr("lxd_cached_image.img2", "aliases.#", "2"),
 					resource.TestCheckTypeSetElemAttr("lxd_cached_image.img2", "aliases.*", alias1),
@@ -83,11 +83,11 @@ func TestAccCachedImage_copiedAliases(t *testing.T) {
 			{
 				Config: testAccCachedImage_copiedAliases(alias1, alias2),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("lxd_cached_image.img3", "source_remote", "ubuntu-minimal-daily"),
-					resource.TestCheckResourceAttr("lxd_cached_image.img3", "source_image", "22.04"),
+					resource.TestCheckResourceAttr("lxd_cached_image.img3", "source_remote", acctest.TestCachedImageSourceRemote),
+					resource.TestCheckResourceAttr("lxd_cached_image.img3", "source_image", acctest.TestCachedImageSourceImage),
 					resource.TestCheckResourceAttr("lxd_cached_image.img3", "copy_aliases", "true"),
 					resource.TestCheckResourceAttr("lxd_cached_image.img3", "aliases.#", "3"),
-					resource.TestCheckTypeSetElemAttr("lxd_cached_image.img3", "aliases.*", "22.04"),
+					resource.TestCheckTypeSetElemAttr("lxd_cached_image.img3", "aliases.*", acctest.TestCachedImageSourceImage),
 					resource.TestCheckTypeSetElemAttr("lxd_cached_image.img3", "aliases.*", alias1),
 					resource.TestCheckTypeSetElemAttr("lxd_cached_image.img3", "aliases.*", alias2),
 					resource.TestCheckResourceAttr("lxd_cached_image.img3", "copied_aliases.#", "10"),
@@ -105,11 +105,11 @@ func TestAccCachedImage_aliasCollision(t *testing.T) {
 			{
 				Config: testAccCachedImage_aliasCollision(),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("lxd_cached_image.img4", "source_remote", "ubuntu-minimal-daily"),
-					resource.TestCheckResourceAttr("lxd_cached_image.img4", "source_image", "22.04"),
+					resource.TestCheckResourceAttr("lxd_cached_image.img4", "source_remote", acctest.TestCachedImageSourceRemote),
+					resource.TestCheckResourceAttr("lxd_cached_image.img4", "source_image", acctest.TestCachedImageSourceImage),
 					resource.TestCheckResourceAttr("lxd_cached_image.img4", "copy_aliases", "true"),
 					resource.TestCheckResourceAttr("lxd_cached_image.img4", "aliases.#", "1"),
-					resource.TestCheckResourceAttr("lxd_cached_image.img4", "aliases.0", "22.04/amd64"),
+					resource.TestCheckResourceAttr("lxd_cached_image.img4", "aliases.0", acctest.TestCachedImageSourceImage+"/amd64"),
 					resource.TestCheckResourceAttr("lxd_cached_image.img4", "copied_aliases.#", "10"),
 				),
 			},
@@ -127,8 +127,8 @@ func TestAccCachedImage_aliasExists(t *testing.T) {
 			{
 				Config: testAccCachedImage_aliasExists1(alias),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("lxd_cached_image.exists1", "source_remote", "ubuntu-minimal-daily"),
-					resource.TestCheckResourceAttr("lxd_cached_image.exists1", "source_image", "22.04"),
+					resource.TestCheckResourceAttr("lxd_cached_image.exists1", "source_remote", acctest.TestCachedImageSourceRemote),
+					resource.TestCheckResourceAttr("lxd_cached_image.exists1", "source_image", acctest.TestCachedImageSourceImage),
 					resource.TestCheckResourceAttr("lxd_cached_image.exists1", "copy_aliases", "false"),
 					resource.TestCheckResourceAttr("lxd_cached_image.exists1", "aliases.#", "1"),
 					resource.TestCheckResourceAttr("lxd_cached_image.exists1", "aliases.0", alias),
@@ -139,8 +139,8 @@ func TestAccCachedImage_aliasExists(t *testing.T) {
 				Config:      testAccCachedImage_aliasExists2(alias),
 				ExpectError: regexp.MustCompile(fmt.Sprintf(`Image alias %q already exists`, alias)),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("lxd_cached_image.exists1", "source_remote", "ubuntu-minimal-daily"),
-					resource.TestCheckResourceAttr("lxd_cached_image.exists1", "source_image", "22.04"),
+					resource.TestCheckResourceAttr("lxd_cached_image.exists1", "source_remote", acctest.TestCachedImageSourceRemote),
+					resource.TestCheckResourceAttr("lxd_cached_image.exists1", "source_image", acctest.TestCachedImageSourceImage),
 					resource.TestCheckResourceAttr("lxd_cached_image.exists1", "aliases.#", "1"),
 					resource.TestCheckResourceAttr("lxd_cached_image.exists1", "aliases.0", alias),
 				),
@@ -160,8 +160,8 @@ func TestAccCachedImage_addRemoveAlias(t *testing.T) {
 			{
 				Config: testAccCachedImage_aliases(alias1),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("lxd_cached_image.img2", "source_remote", "ubuntu-minimal-daily"),
-					resource.TestCheckResourceAttr("lxd_cached_image.img2", "source_image", "22.04"),
+					resource.TestCheckResourceAttr("lxd_cached_image.img2", "source_remote", acctest.TestCachedImageSourceRemote),
+					resource.TestCheckResourceAttr("lxd_cached_image.img2", "source_image", acctest.TestCachedImageSourceImage),
 					resource.TestCheckResourceAttr("lxd_cached_image.img2", "copy_aliases", "false"),
 					resource.TestCheckResourceAttr("lxd_cached_image.img2", "aliases.#", "1"),
 					resource.TestCheckResourceAttr("lxd_cached_image.img2", "aliases.0", alias1),
@@ -171,8 +171,8 @@ func TestAccCachedImage_addRemoveAlias(t *testing.T) {
 			{
 				Config: testAccCachedImage_aliases(alias1, alias2),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("lxd_cached_image.img2", "source_remote", "ubuntu-minimal-daily"),
-					resource.TestCheckResourceAttr("lxd_cached_image.img2", "source_image", "22.04"),
+					resource.TestCheckResourceAttr("lxd_cached_image.img2", "source_remote", acctest.TestCachedImageSourceRemote),
+					resource.TestCheckResourceAttr("lxd_cached_image.img2", "source_image", acctest.TestCachedImageSourceImage),
 					resource.TestCheckResourceAttr("lxd_cached_image.img2", "copy_aliases", "false"),
 					resource.TestCheckResourceAttr("lxd_cached_image.img2", "aliases.#", "2"),
 					resource.TestCheckTypeSetElemAttr("lxd_cached_image.img2", "aliases.*", alias1),
@@ -183,8 +183,8 @@ func TestAccCachedImage_addRemoveAlias(t *testing.T) {
 			{
 				Config: testAccCachedImage_aliases(alias2),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("lxd_cached_image.img2", "source_remote", "ubuntu-minimal-daily"),
-					resource.TestCheckResourceAttr("lxd_cached_image.img2", "source_image", "22.04"),
+					resource.TestCheckResourceAttr("lxd_cached_image.img2", "source_remote", acctest.TestCachedImageSourceRemote),
+					resource.TestCheckResourceAttr("lxd_cached_image.img2", "source_image", acctest.TestCachedImageSourceImage),
 					resource.TestCheckResourceAttr("lxd_cached_image.img2", "copy_aliases", "false"),
 					resource.TestCheckResourceAttr("lxd_cached_image.img2", "aliases.#", "1"),
 					resource.TestCheckResourceAttr("lxd_cached_image.img2", "aliases.0", alias2),
@@ -206,8 +206,8 @@ func TestAccCachedImage_project(t *testing.T) {
 				Config: testAccCachedImage_project(projectName),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("lxd_project.project1", "name", projectName),
-					resource.TestCheckResourceAttr("lxd_cached_image.img1", "source_remote", "ubuntu-minimal-daily"),
-					resource.TestCheckResourceAttr("lxd_cached_image.img1", "source_image", "22.04"),
+					resource.TestCheckResourceAttr("lxd_cached_image.img1", "source_remote", acctest.TestCachedImageSourceRemote),
+					resource.TestCheckResourceAttr("lxd_cached_image.img1", "source_image", acctest.TestCachedImageSourceImage),
 					resource.TestCheckResourceAttr("lxd_cached_image.img1", "project", projectName),
 					resource.TestCheckNoResourceAttr("lxd_cached_image.img1", "aliases"),
 					resource.TestCheckResourceAttr("lxd_cached_image.img1", "copied_aliases.#", "0"),
@@ -218,86 +218,86 @@ func TestAccCachedImage_project(t *testing.T) {
 }
 
 func testAccCachedImage_basic() string {
-	return `
+	return fmt.Sprintf(`
 resource "lxd_cached_image" "img1" {
-  source_remote = "ubuntu-minimal-daily"
-  source_image  = "22.04"
+  source_remote = "%s"
+  source_image  = "%s"
   copy_aliases  = true
 }
-	`
+	`, acctest.TestCachedImageSourceRemote, acctest.TestCachedImageSourceImage)
 }
 
 func testAccCachedImage_basicVM() string {
-	return `
+	return fmt.Sprintf(`
 resource "lxd_cached_image" "img1vm" {
-  source_remote = "ubuntu-minimal-daily"
-  source_image  = "22.04"
+  source_remote = "%s"
+  source_image  = "%s"
   type          = "virtual-machine"
   copy_aliases  = true
 }
-	`
+	`, acctest.TestCachedImageSourceRemote, acctest.TestCachedImageSourceImage)
 }
 
 func testAccCachedImage_aliases(aliases ...string) string {
 	return fmt.Sprintf(`
 resource "lxd_cached_image" "img2" {
-  source_remote = "ubuntu-minimal-daily"
-  source_image  = "22.04"
+  source_remote = "%s"
+  source_image  = "%s"
   aliases       = ["%s"]
   copy_aliases  = false
 }
-	`, strings.Join(aliases, `","`))
+	`, acctest.TestCachedImageSourceRemote, acctest.TestCachedImageSourceImage, strings.Join(aliases, `","`))
 }
 
 func testAccCachedImage_aliasExists1(alias string) string {
 	return fmt.Sprintf(`
 resource "lxd_cached_image" "exists1" {
-  source_remote = "ubuntu-minimal-daily"
-  source_image  = "22.04"
+  source_remote = "%s"
+  source_image  = "%s"
   aliases       = ["%s"]
   copy_aliases  = false
 }
-	`, alias)
+	`, acctest.TestCachedImageSourceRemote, acctest.TestCachedImageSourceImage, alias)
 }
 
 func testAccCachedImage_aliasExists2(alias string) string {
 	return fmt.Sprintf(`
 resource "lxd_cached_image" "exists1" {
-  source_remote = "ubuntu-minimal-daily"
-  source_image  = "22.04"
+  source_remote = "%s"
+  source_image  = "%s"
   aliases       = ["%s"]
   copy_aliases  = false
 }
 
 resource "lxd_cached_image" "exists2" {
-  source_remote = "ubuntu-minimal-daily"
-  source_image  = "22.04"
+  source_remote = "%s"
+  source_image  = "%s"
   aliases       = ["%s"]
   copy_aliases  = false
 }
-	`, alias, alias)
+	`, acctest.TestCachedImageSourceRemote, acctest.TestCachedImageSourceImage, alias, acctest.TestCachedImageSourceRemote, acctest.TestCachedImageSourceImage, alias)
 }
 
 func testAccCachedImage_copiedAliases(aliases ...string) string {
 	return fmt.Sprintf(`
 resource "lxd_cached_image" "img3" {
-  source_remote = "ubuntu-minimal-daily"
-  source_image  = "22.04"
-  aliases       = ["22.04","%s"]
+  source_remote = "%s"
+  source_image  = "%s"
+  aliases       = ["%s","%s"]
   copy_aliases  = true
 }
-	`, strings.Join(aliases, `","`))
+	`, acctest.TestCachedImageSourceRemote, acctest.TestCachedImageSourceImage, acctest.TestCachedImageSourceImage, strings.Join(aliases, `","`))
 }
 
 func testAccCachedImage_aliasCollision() string {
-	return `
+	return fmt.Sprintf(`
 resource "lxd_cached_image" "img4" {
-  source_remote = "ubuntu-minimal-daily"
-  source_image  = "22.04"
-  aliases       = ["22.04/amd64"]
+  source_remote = "%s"
+  source_image  = "%s"
+  aliases       = ["%s/amd64"]
   copy_aliases  = true
 }
-	`
+	`, acctest.TestCachedImageSourceRemote, acctest.TestCachedImageSourceImage, acctest.TestCachedImageSourceImage)
 }
 
 func testAccCachedImage_project(project string) string {
@@ -306,9 +306,9 @@ resource "lxd_project" "project1" {
   name = "%s"
 }
 resource "lxd_cached_image" "img1" {
-  source_remote = "ubuntu-minimal-daily"
-  source_image  = "22.04"
+  source_remote = "%s"
+  source_image  = "%s"
   project       = lxd_project.project1.name
 }
-	`, project)
+	`, project, acctest.TestCachedImageSourceRemote, acctest.TestCachedImageSourceImage)
 }
