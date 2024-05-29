90a3b16a4bc06af77fc042f50579299314ed8d7e
Din Music <din.music@canonical.com>
2024-05-29 13:38:18 +0000

    internal/storage/pool_test: Add tests for storage pool source
    Signed-off-by: Din Music <din.music@canonical.com>

--- internal/storage/resource_storage_pool_test.go
+++ internal/storage/resource_storage_pool_test.go
@@ -1,7 +1,12 @@
 package storage_test
 
 import (
+	"bytes"
 	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
@@ -176,6 +181,42 @@ func TestAccStoragePool_config(t *testing.T) {
 	})
 }
 
+func TestAccStoragePool_configSource(t *testing.T) {
+	drivers := []string{"dir", "zfs", "btrfs", "lvm"}
+
+	// Skip test if we have no root permissions. Root permissions are required
+	// for creating loopback devices.
+	acctest.PreCheckRoot(t)
+
+	for _, poolDriver := range drivers {
+		poolName := acctest.GenerateName(2, "-")
+		poolSource, cleanup := ensureSource(t, poolDriver)
+
+		t.Run(fmt.Sprintf("%s[%s]", t.Name(), poolDriver), func(t *testing.T) {
+			defer cleanup()
+			resource.Test(t, resource.TestCase{
+				PreCheck:                 func() { acctest.PreCheck(t) },
+				ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories,
+				Steps: []resource.TestStep{
+					{
+						Config: testAccStoragePool_configSource(poolName, poolDriver, poolSource),
+						Check: resource.ComposeTestCheckFunc(
+							resource.TestCheckResourceAttr("lxd_storage_pool.storage_pool1", "name", poolName),
+							resource.TestCheckResourceAttr("lxd_storage_pool.storage_pool1", "driver", poolDriver),
+							resource.TestCheckResourceAttr("lxd_storage_pool.storage_pool1", "source", poolSource),
+							resource.TestCheckNoResourceAttr("lxd_storage_pool.storage_pool1", "config.source"),
+						),
+					},
+					{
+						// Reapply the same config to ensure there is no drift in terraform state.
+						Config: testAccStoragePool_configSource(poolName, poolDriver, poolSource),
+					},
+				},
+			})
+		})
+	}
+}
+
 func TestAccStoragePool_project(t *testing.T) {
 	poolName := acctest.GenerateName(2, "-")
 	projectName := acctest.GenerateName(2, "-")
@@ -319,6 +360,16 @@ resource "lxd_storage_pool" "storage_pool1" {
 	`, name, driver)
 }
 
+func testAccStoragePool_configSource(name string, driver string, source string) string {
+	return fmt.Sprintf(`
+resource "lxd_storage_pool" "storage_pool1" {
+  name   = "%s"
+  driver = "%s"
+  source = "%s"
+}
+	`, name, driver, source)
+}
+
 func testAccStoragePool_project(name string, driver string, project string) string {
 	return fmt.Sprintf(`
 resource "lxd_project" "project1" {
@@ -361,4 +412,63 @@ resource "lxd_storage_pool" "storage_pool1" {
   driver = "%[2]s"
 }
 	`, name, driver)
+}
+
+// ensureSource ensures temporary storage pool source is created based on the provided
+// storage pool driver. For "dir", a temporary directory is created. For "zfs", "lvm",
+// and "btrfs" a loopback device pointing to a temporary file is created.
+func ensureSource(t *testing.T, driver string) (source string, cleanup func()) {
+	switch driver {
+	case "dir":
+		// For directory storage driver, just create an empty directory.
+
+		source = filepath.Join(os.TempDir(), "tf-storage-pool-dir")
+		_ = os.RemoveAll(source)
+
+		err := os.Mkdir(source, os.ModePerm)
+		if err != nil {
+			t.Fatalf("Failed to create temporary directory: %v", err)
+		}
+
+		cleanup = func() {
+			_ = os.RemoveAll(source)
+		}
+	case "zfs", "btrfs", "lvm":
+		// For zfs, btrfs, and lvm storage drivers, create a temporary file
+		// and attach it as a loopback device.
+
+		disk, err := os.CreateTemp(os.TempDir(), "tf-storage-pool-disk-*")
+		if err != nil {
+			t.Fatalf("Failed to create temporary disk file: %v", err)
+		}
+		defer disk.Close()
+
+		err = disk.Truncate(128 * 1024 * 1024) // 128 MiB
+		if err != nil {
+			t.Fatalf("Failed to truncate temporary disk file: %v", err)
+		}
+
+		// Create the loopback device.
+		var bufOut, bufErr bytes.Buffer
+		cmd := exec.Command("sudo", "losetup", "-fP", "--show", disk.Name())
+		cmd.Stdout = &bufOut
+		cmd.Stderr = &bufErr
+		err = cmd.Run()
+		if err != nil {
+			t.Fatalf("Failed to create loopback device (%v): %s", err, bufErr.String())
+		}
+
+		// Retrieve the loopback device name.
+		source = strings.TrimSpace(bufOut.String())
+
+		cleanup = func() {
+			// Detach loopback device and remove temporary file.
+			_ = exec.Command("sudo", "losetup", "-d", source).Run()
+			_ = os.RemoveAll(disk.Name())
+		}
+	default:
+		t.Fatalf("Cannot create source for storage driver %q", driver)
+	}
+
+	return source, cleanup
 }
