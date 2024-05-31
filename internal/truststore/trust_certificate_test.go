491ecef2b7c3d9d2497c396a115d03102067a52b
Din Music <din.music@canonical.com>
2024-05-31 19:48:16 +0000

    internal/truststore: Add test for certificate type
    Signed-off-by: Din Music <din.music@canonical.com>

--- internal/truststore/trust_certificate_test.go
+++ internal/truststore/trust_certificate_test.go
@@ -25,6 +25,7 @@ func TestAccTrustCertificate_content(t *testing.T) {
 				Config: testAccTrustCertificate_content(certName, cert),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("lxd_trust_certificate.cert", "name", certName),
+					resource.TestCheckResourceAttr("lxd_trust_certificate.cert", "type", "client"),
 					resource.TestCheckResourceAttr("lxd_trust_certificate.cert", "content", cert),
 					resource.TestCheckResourceAttr("lxd_trust_certificate.cert", "fingerprint", fingerprint),
 					// Ensure path is not set.
@@ -55,9 +56,41 @@ func TestAccTrustCertificate_path(t *testing.T) {
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("lxd_trust_certificate.cert", "name", certName),
 					resource.TestCheckResourceAttr("lxd_trust_certificate.cert", "path", certPath),
+					resource.TestCheckResourceAttr("lxd_trust_certificate.cert", "type", "client"),
 					resource.TestCheckResourceAttr("lxd_trust_certificate.cert", "fingerprint", fingerprint),
 					// Ensure content is not set.
 					resource.TestCheckNoResourceAttr("lxd_trust_certificate.cert", "content"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccTrustCertificate_type(t *testing.T) {
+	certName := acctest.GenerateName(2, "-")
+	cert, fingerprint := generateCert(t)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(t) },
+		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccTrustCertificate_type(certName, "client", cert),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("lxd_trust_certificate.cert", "name", certName),
+					resource.TestCheckResourceAttr("lxd_trust_certificate.cert", "type", "client"),
+					resource.TestCheckResourceAttr("lxd_trust_certificate.cert", "content", cert),
+					resource.TestCheckResourceAttr("lxd_trust_certificate.cert", "fingerprint", fingerprint),
+					resource.TestCheckResourceAttr("lxd_trust_certificate.cert", "projects.#", "0"),
+				),
+			},
+			{
+				Config: testAccTrustCertificate_type(certName, "metrics", cert),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("lxd_trust_certificate.cert", "name", certName),
+					resource.TestCheckResourceAttr("lxd_trust_certificate.cert", "type", "metrics"),
+					resource.TestCheckResourceAttr("lxd_trust_certificate.cert", "content", cert),
+					resource.TestCheckResourceAttr("lxd_trust_certificate.cert", "fingerprint", fingerprint),
 				),
 			},
 		},
@@ -144,6 +177,19 @@ resource "lxd_trust_certificate" "cert" {
   projects = [%s]
 }
 	`, name, certPath, acctest.QuoteStrings(projects))
+}
+
+func testAccTrustCertificate_type(name string, certType string, cert string, projects ...string) string {
+	return fmt.Sprintf(`
+resource "lxd_trust_certificate" "cert" {
+  name     = "%s"
+  type     = "%s"
+  content = <<-EOF
+%s
+EOF
+  projects = [%s]
+}
+	`, name, certType, strings.TrimRight(cert, "\n"), acctest.QuoteStrings(projects))
 }
 
 func generateCert(t *testing.T) (certificate string, fingerprint string) {
