5d079f4d870108b1cc428196e488c09955ec0c1a
Fabian Mettler <dev@maveonair.com>
2024-08-01 15:08:22 +0200

    image: Architecture becomes an argument
    Signed-off-by: Fabian Mettler <dev@maveonair.com>

---
+++ internal/image/schema_validators.go
@@ -0,0 +1,39 @@
+package image
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+
+	"github.com/lxc/incus/v6/shared/osarch"
+)
+
+type architectureValidator struct{}
+
+func (v architectureValidator) Description(ctx context.Context) string {
+	supportedArchitecturesList := strings.Join(osarch.SupportedArchitectures(), ", ")
+	return fmt.Sprintf("Attribute architecture value must be one of: %s.", supportedArchitecturesList)
+}
+
+func (v architectureValidator) MarkdownDescription(ctx context.Context) string {
+	return v.Description(ctx)
+}
+
+func (v architectureValidator) ValidateString(ctx context.Context, req validator.StringRequest, resp *validator.StringResponse) {
+	value := req.ConfigValue.ValueString()
+	if value == "" {
+		return
+	}
+
+	for _, supportedArchitecture := range osarch.SupportedArchitectures() {
+		if value == supportedArchitecture {
+			return
+		}
+	}
+
+	resp.Diagnostics.AddAttributeError(req.Path, "Invalid architecture",
+		v.Description(ctx),
+	)
+}
