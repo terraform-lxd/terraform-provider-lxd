a27fb9902187cfd25556b8d3b0a573590c0ca12c
Din Music <din.music@canonical.com>
2024-08-23 12:55:07 +0000

    internal/provider-config: Add tests for DetermineLXDAddress
    Signed-off-by: Din Music <din.music@canonical.com>

---
+++ internal/provider-config/config_test.go
@@ -0,0 +1,90 @@
+package config
+
+import "testing"
+
+func TestDetermineLXDAddress(t *testing.T) {
+	tests := []struct {
+		Name      string
+		Protocol  string
+		Address   string
+		Expect    string
+		ExpectErr bool
+	}{
+		{
+			Name:    "Empty address",
+			Address: "",
+			Expect:  "unix://",
+		},
+		{
+			Name:    "Address starts with /",
+			Address: "/path/to/socket",
+			Expect:  "unix:///path/to/socket",
+		},
+		{
+			Name:    "Address does not start with /",
+			Address: "localhost:8443",
+			Expect:  "https://localhost:8443",
+		},
+		{
+			Name:    "Only hostname | Protocol lxd",
+			Address: "localhost",
+			Expect:  "https://localhost:8443",
+		},
+		{
+			Name:     "Only hostname | Protocol simplestreams",
+			Protocol: "simplestreams",
+			Address:  "localhost",
+			Expect:   "https://localhost:443",
+		},
+		{
+			Name:    "Scheme and hostname | Protocol lxd",
+			Address: "https://localhost",
+			Expect:  "https://localhost:8443",
+		},
+		{
+			Name:     "Scheme and hostname | Protocol simplestreams",
+			Protocol: "simplestreams",
+			Address:  "https://localhost",
+			Expect:   "https://localhost:443",
+		},
+		{
+			Name:    "Scheme, hostname, port | URL",
+			Address: "https://localhost:1234",
+			Expect:  "https://localhost:1234",
+		},
+		// Expected errors.
+		{
+			Name:      "Unsupported simplestreams scheme",
+			Protocol:  "simplestreams",
+			Address:   "/path/to/socket",
+			ExpectErr: true,
+		},
+		{
+			Name:      "Missing hostname",
+			Address:   "https://:8443",
+			ExpectErr: true,
+		},
+		{
+			Name:      "Unsupported scheme",
+			Address:   "http://localhost:8443",
+			ExpectErr: true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			addr, err := DetermineLXDAddress(test.Protocol, test.Address)
+			if err != nil && !test.ExpectErr {
+				t.Fatalf("Unexpected error: %v", err)
+			}
+
+			if err == nil && test.ExpectErr {
+				t.Fatalf("Expected an error, but got none")
+			}
+
+			if addr != test.Expect {
+				t.Fatalf("Expected address %q, got %q", test.Expect, addr)
+			}
+		})
+	}
+}
