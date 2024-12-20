155d25d1a0d5665a1edd8670a2128f79c6b4ed88
Din Music <din.music@canonical.com>
2024-12-20 15:35:02 +0000

    internal/instance: Add missing virtualization precheck
    Signed-off-by: Din Music <din.music@canonical.com>

--- internal/instance/resource_instance_test.go
+++ internal/instance/resource_instance_test.go
@@ -568,7 +568,10 @@ func TestAccInstance_fileUploadVirtualMachine(t *testing.T) {
 	instanceName := acctest.GenerateName(2, "-")
 
 	resource.Test(t, resource.TestCase{
-		PreCheck:                 func() { acctest.PreCheck(t) },
+		PreCheck: func() {
+			acctest.PreCheck(t)
+			acctest.PreCheckVirtualization(t)
+		},
 		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			{
