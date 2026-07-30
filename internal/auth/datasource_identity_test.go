16635976d3424b90fdcaef245ae0cb62a9f53366
Din Music <din.music@canonical.com>
2026-07-30 21:23:36 +0000

    auth/ds_identity: Test pending TLS identity
    Signed-off-by: Din Music <din.music@canonical.com>

--- internal/auth/datasource_identity_test.go
+++ internal/auth/datasource_identity_test.go
@@ -24,6 +24,7 @@ func TestAccIdentity_DS_bearer(t *testing.T) {
 					resource.TestCheckResourceAttr("data.lxd_auth_identity.identity", "name", identity),
 					resource.TestCheckResourceAttr("data.lxd_auth_identity.identity", "auth_method", "bearer"),
 					resource.TestCheckResourceAttr("data.lxd_auth_identity.identity", "groups.#", "0"),
+					resource.TestCheckResourceAttrSet("data.lxd_auth_identity.identity", "identifier"),
 				),
 			},
 			{
@@ -64,6 +65,7 @@ func TestAccIdentity_DS_tls(t *testing.T) {
 					resource.TestCheckResourceAttr("data.lxd_auth_identity.identity", "auth_method", "tls"),
 					resource.TestCheckResourceAttr("data.lxd_auth_identity.identity", "groups.#", "0"),
 					resource.TestCheckResourceAttrSet("data.lxd_auth_identity.identity", "tls_certificate"),
+					resource.TestCheckResourceAttrSet("data.lxd_auth_identity.identity", "identifier"),
 				),
 			},
 			{
@@ -79,6 +81,41 @@ func TestAccIdentity_DS_tls(t *testing.T) {
 			},
 		},
 	})
+}
+
+func TestAccIdentity_DS_tlsPending(t *testing.T) {
+	identity := acctest.GenerateName(2, "-")
+	dataSourceName := "data.lxd_auth_identity.identity"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			acctest.PreCheck(t)
+			acctest.PreCheckAPIExtensions(t, "access_management", "access_management_tls")
+		},
+		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				// A pending identity has no certificate, and its identifier is a UUID that LXD replaces with
+				// the certificate fingerprint once the trust token is redeemed.
+				Config: acctest.Provider() + testAccIdentity_DS_tlsPending(identity),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(dataSourceName, "name", identity),
+					resource.TestCheckResourceAttr(dataSourceName, "auth_method", "tls"),
+					resource.TestCheckResourceAttr(dataSourceName, "tls_certificate", ""),
+					resource.TestCheckResourceAttrSet(dataSourceName, "identifier"),
+				),
+			},
+		},
+	})
+}
+
+func testAccIdentity_DS_tlsPending(name string) string {
+	return testAccIdentity_tlsPending(name, []string{}) + `
+		data "lxd_auth_identity" "identity" {
+		  auth_method = "tls"
+		  name        = lxd_auth_identity.identity.name
+		}
+	`
 }
 
 func testAccIdentity_DS_bearer(name string, groups []string) string {
